ba8e877de841af6640880d57878081ac3137fb8f
GustavonBrito <gustavoserafio@gmail.com>
2024-05-19 16:37:53 -0300

    feat: implementation of exercises page 1

---
+++ src/main/java/com/bichinhos/exerciciosExtraCurriculares/ExerciciosExtraCurricularesApplication.java
@@ -0,0 +1,13 @@
+package com.bichinhos.exerciciosExtraCurriculares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciciosExtraCurricularesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciciosExtraCurricularesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bichinhos/exerciciosExtraCurriculares/ExerciciosExtraCurricularesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bichinhos.exerciciosExtraCurriculares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciciosExtraCurricularesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
